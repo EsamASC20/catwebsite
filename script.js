450c01da1f87c2dcc941c1fa75a5b837e971cca1
EsamASC21 <esama.ny23@gmail.com>
2021-08-18 18:58:09 -0400

    Update 1.4

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
 let database = firebase.database().ref();
+let myJson = [];
 
-database.on("child_added",addMessageTB);
-function addMessageTB(rowData){
-    let row = rowData.val(); // Returns an object just like the "value" we pushed
+let button = document.querySelector("#press")
+
+button.onclick = function(event) {
+    
     let board = document.querySelector("#charactersList")
     let data = document.createElement("li");
+    
+    event.preventDefault();
+    board.innerHTML = "";
+    console.log("Hello");
+    database.on("child_added",addMessageTB);
+    function addMessageTB(rowData){
+        let row = rowData.val(); // Returns an object just like the "value" we pushed
 
-    board.innerHTML += '<li class="character"> '+ '<p>' + row.DATA + '</p>' + '<br>' + '<h2>' + row.NAME + '</h2>'  + '<img src ="' +row.IMG +'">' + ' </li>';
-    board.appendChild(data);
+        let userInput = document.querySelector("#searchBar")
+        if (row.NAME == userInput.value) {
 
+            board.innerHTML += '<li class="character"> '+ '<p>' + row.DATA + '</p>' + '<br>' + '<h2>' + row.NAME + '</h2>'  + '<img class=productImg src ="' +row.IMG +'">' + ' </li>';
+            board.appendChild(data);
+        }
+
+    }
 }
-
-
-
-
-// const charactersList = document.getElementById('charactersList');
-// const searchBar = document.getElementById('searchBar');
-// let hpCharacters = [];
-
-// searchBar.addEventListener('keyup', (e) => {
-//     const searchString = e.target.value.toLowerCase();
-
-//     const filteredCharacters = hpCharacters.filter((character) => {
-//         return (
-//             character.name.toLowerCase().includes(searchString) ||
-//             character.house.toLowerCase().includes(searchString)
-//         );
-//     });
-//     displayCharacters(filteredCharacters);
-// });
+ 
